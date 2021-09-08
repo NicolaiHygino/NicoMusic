117a02db4bf8c4ab5806a2446b76d36f84338b59
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-08 20:30:45 -0300

    setting environments variables and build project for deploy

--- src/services/spotifyApi/Authentication/index.js
+++ src/services/spotifyApi/Authentication/index.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import { objectToURLParam } from 'services/spotifyApi/objectToUrlParam';
 
 const params = objectToURLParam({
-  client_id: 'a1a6e3d9fe3441f2827747335adcfdfa',
+  client_id: process.env.REACT_APP_CLIENT_ID,
   response_type: 'code',
-  redirect_uri: 'http://localhost:3000/',
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
   scope: 'streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-library-read user-library-modify user-read-recently-played',
 });
 
 export const AUTH_URL = `https://accounts.spotify.com/authorize?${params}`;
 
 export const getSpotifyTokens = async (code) => {
-  const result = await axios.post('http://localhost:3001/login', {code});
+  const result = await axios.post(process.env.REACT_APP_SERVER_URL, {code});
   return result;
 };
