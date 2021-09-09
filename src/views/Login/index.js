ef9ac99c175b112fe91a1c7e47c601b20d8e2dd4
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-08 21:16:28 -0300

    fixing netlify error test

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { 
   AUTH_URL,
   getSpotifyTokens 
-} from "services/spotifyApi/authentication/index";
+} from "../../services/spotifyApi/authentication/index";
 import { LoginButton, CenteredContainer } from './style';
 import { useHistory } from 'react-router-dom';
 
