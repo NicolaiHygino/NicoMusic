a1a0446dbf11221a6c84b1d5be25d60256188d35
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-15 09:54:06 -0300

    deleting unused import in mobile player

--- src/components/MobilePlayer/index.js
+++ src/components/MobilePlayer/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { transferUserPlayback } from 'services/spotifyApi/endpoints';
 import PopUp from './PopUp';
 import MobileTrackInfo from './MobileTrackInfo';
 import MobileMainControls from './MobileMainControls';
