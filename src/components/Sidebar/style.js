25da6054cda1c8de33e369eab70850a0a289625f
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-16 12:24:20 -0300

    feat: define flexblox to nav and flex to its children

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom';
 
 export const StyledSidebar = styled.nav`
+  display: flex;
+  flex-direction: column;
   width: 200px;
   background-color: black;
   height: 100%;
@@ -10,6 +12,7 @@ export const StyledSidebar = styled.nav`
 
 export const LogoWrapper = styled.div`
   display: flex;
+  flex: 40px;
   align-items: center;
   justify-content: center;
   padding: 0px 10px;
@@ -21,6 +24,7 @@ export const LogoWrapper = styled.div`
 
 export const Nav = styled.ul`
   margin: 20px 0;
+  flex: 1;
 `;
 
 export const StyledNavItem = styled.li`
@@ -47,4 +51,8 @@ export const NavItemWrapper = styled.div`
     font-size: .87em;
     font-weight: 600;
   }
+`;
+
+export const UserPlaylistsWrapper = styled.div`
+  overflow: scroll;
 `;
