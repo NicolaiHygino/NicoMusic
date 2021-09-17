11bc389cc542dce02de535cd0ebe31189b9ab6ea
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-16 21:20:00 -0300

    feat: new NextArrow and PrevArrow icons

---
+++ src/assets/Icons/NextArrowIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NextArrowIcon = () => (
+  <svg
+    role="img"
+    focusable="false"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24"
+  >
+    <polyline points="8 4 17 12 8 20" fill="none" stroke="#181818"></polyline>
+  </svg>
+);
+
+export default NextArrowIcon;
---
+++ src/assets/Icons/PrevArrowIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PrevArrowIcon = () => (
+  <svg
+    role="img"
+    focusable="false"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24"
+  >
+    <polyline points="16 4 7 12 16 20" fill="none" stroke="#181818"></polyline>
+  </svg>
+);
+
+export default PrevArrowIcon;
