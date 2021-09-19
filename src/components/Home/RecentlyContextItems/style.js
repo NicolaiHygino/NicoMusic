4d1efd43dca6172a0783b6a7d41bde885420ef36
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-19 14:50:46 -0300

    fix: image always square in RecentlyPlayedContexts

--- src/components/Home/RecentlyContextItems/style.js
+++ src/components/Home/RecentlyContextItems/style.js
@@ -40,7 +40,9 @@ export const ImageWrapper = styled.div`
   min-width: 80px;
  
   & img {
+    object-fit: cover;
     width: 100%;
+    height: 100%;
   }
 
   @media screen and (max-width: 550px) {
