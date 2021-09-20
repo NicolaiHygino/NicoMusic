b873d9d7112ef6d573ffafe7a406ff576be236cb
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-20 10:19:54 -0300

    feat: UsePlaylistsSection fetch storaged payload if available

--- src/components/Home/UserPlaylistsSection/index.js
+++ src/components/Home/UserPlaylistsSection/index.js
@@ -7,13 +7,14 @@ import {
   SectionHeader,
 } from 'globalStyles';
 import { ItemsContainer } from '../style';
+import { fetchStorage } from 'utils/fetchStorage';
 
 const getRandomItems = (array, length) => {
   let randItems = [];
   while (randItems.length < length) {
     let randNum = Math.floor(Math.random() * array.length);
-    if (randItems.every(item => item.name !== array[randNum].name)) {
-      randItems.push(array[randNum])
+    if (randItems.every((item) => item.name !== array[randNum].name)) {
+      randItems.push(array[randNum]);
     }
   }
   return randItems;
@@ -23,26 +24,22 @@ const UserPlaylistSection = ({ token }) => {
   const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
-    getUserPlaylists(token, 50)
-      .then(res => {
-        setPlaylists(getRandomItems(res.data.items, 5));
-      })
+    fetchStorage(token, getUserPlaylists, 'user-playlists-home')
+      .then(res => setPlaylists(getRandomItems(res.data.items, 5)));
   }, [token]);
 
   if (!playlists) return null;
   return (
     <SectionWrapper>
       <SectionHeaderWrapper>
-        <SectionHeader>
-          Your favorite playlists
-        </SectionHeader>
+        <SectionHeader>Your favorite playlists</SectionHeader>
       </SectionHeaderWrapper>
       <ItemsContainer>
-        { playlists.map(playlist => 
+        {playlists.map((playlist) => (
           <SectionItem key={playlist.id} item={playlist} />
-        )}
+        ))}
       </ItemsContainer>
-    </SectionWrapper>  
+    </SectionWrapper>
   );
 };
 
