b358fb0ec408ae20ccf03f20c3bd339b83620989
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-09-27 14:11:46 -0300

    feat: TrackInfo's artist name link to artist page

--- src/components/Player/TrackInfo/index.js
+++ src/components/Player/TrackInfo/index.js
@@ -4,10 +4,12 @@ import {
   ImageWrapper,
   TitleWrapper
 } from './style'
-
+import { Link } from 'react-router-dom';
 
 const TrackInfo = ({ track }) => {
-  if (!track) return <MusicInfo></MusicInfo>
+  if (!track) return <MusicInfo></MusicInfo>;
+  const artistId = track.artists[0].uri.split(':')[2];
+  
   return (
     <MusicInfo>
       <ImageWrapper>
@@ -15,7 +17,9 @@ const TrackInfo = ({ track }) => {
       </ImageWrapper>
       <TitleWrapper>
         <p>{track.name}</p>
-        <span>{track.artists[0].name}</span>
+        <Link to={`/artist/${artistId}`}>
+          <span>{track.artists[0].name}</span>
+        </Link>
       </TitleWrapper>
     </MusicInfo>
   );
